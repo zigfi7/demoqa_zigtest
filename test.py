fae0afe1acb4fc23e915a606652e7093f2918850
Artur Kujawa <zigfi7@gmail.com>
2021-12-06 00:57:50 +0000

    Added quick user creation and authentication test 2021-12-06_00-57-50_143926832

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 #!/bin/python3
 import requests
 import json
+import time
 
-def getf(url):
-	response = requests.get(url)
+def echoz(string):
+	print (str(time.strftime("%Y_%m_%d_%H_%M_%S"))+': '+string)
+
+def getf(url,token):
+	auth_token = "Bearer %s" % token
+	header = {"Authorization": auth_token}
+	response = requests.get(url,headers=header)
 	return response.text
 
 def putf(url, data):
@@ -14,43 +20,84 @@ def posf(url, data):
 	response = requests.post(url, json=data)
 	return response.text
 
-def delf(url, data):
-	response = requests.delete(url, json=data)
+def delf(url, data, token):
+	auth_token = "Bearer %s" % token
+	header = {"Authorization": auth_token}
+	response = requests.delete(url, json=data, headers=header)
 	return response.text
 
+#---------------------------------------------
 
-
-
-
-
+epoch_time = int(time.time())
 
 urlroot="https://demoqa.com"
+xuser = 'user_' + str(epoch_time)
+xpass = 'pAs!_' + str(epoch_time)
 
-getf (urlroot+"/books")
-getf (urlroot+"/Account/v1/User")
-
-#putf (urlroot+"/books")
-
-data = {"userName": "string","password": "string"}
-
-
-
-output = json.loads(posf (urlroot+"/Account/v1/Authorized",data))
-
-print (json.dumps(output, sort_keys=True, indent=4))
+print ('')
+print ('')
+echoz ('Hello. We going to test '+urlroot+' using username: '+xuser+' and password: '+ xpass)
+print ('')
+print ('')
 
 
-# Account
-#posf (urlroot+"/Account/v1/Authorized",data)
-#posf (urlroot+"/Account/v1/GenerateToken",data)
-#posf (urlroot+"/Account/v1/User",data)
-#delf (urlroot+"/Account/v1/User/{UUID}",data)
-#getf (urlroot+"/Account/v1/User/{UUID}")
+login_data = {"userName": xuser,"password": xpass}
+
+echoz ('Chcecking Autorization')
+output = json.loads(posf (urlroot+"/Account/v1/Authorized",login_data))
+echoz (json.dumps(output))  #, sort_keys=True, indent=4
+
+echoz ('Adding user')
+output = json.loads(posf (urlroot+"/Account/v1/User",login_data))
+echoz (json.dumps(output))  
+userID = output["userID"]
+echoz ("We have now user id: "+userID)  
+
+echoz ('Generate Token')
+output = json.loads(posf (urlroot+"/Account/v1/GenerateToken",login_data))
+echoz (json.dumps(output))  
+token = output["token"]
+echoz ("We have token: "+token)  
+
+echoz ('Chcecking Autorization')
+output = json.loads(posf (urlroot+"/Account/v1/Authorized",login_data))
+echoz (json.dumps(output))  
+
+echoz ('User chceck ')
+output = json.loads(getf (urlroot+"/Account/v1/User/"+userID,token))
+echoz (json.dumps(output))  
+
+
+
+
+
+
+
+
+
+
+echoz ('Deleting user')
+output = delf (urlroot+"/Account/v1/User/"+userID,login_data,token)
+echoz (output)
+
+echoz ('User chceck ')
+output = json.loads(getf (urlroot+"/Account/v1/User/"+userID,token))
+echoz (json.dumps(output))  
+
+
+
+print ('')
+print ('')
+echoz ('finish')
+print ('')
+print ('')
+
+
 
 # BookStore
 #getf (urlroot+"/BookStore/v1/Books")
 #posf (urlroot+"/BookStore/v1/Books",data)
-#delf (urlroot+"/BookStore/v1/Books",data)
+#delf (urlroot+"/BookStore/v1/Books")
 #getf (urlroot+"/BookStore/v1/Book")
 #delf (urlroot+"/BookStore/v1/Book",data)
 #putf (urlroot+" /BookStore/v1/Books/{ISBN}",data)
